2124a86def5ca9ed3e4253280550239422bdbd76
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-09-21 08:31:08 +0300

    Created useGetMessages hook and MessagesSckeleton implemented with the help of Dasy UI

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,22 +1,19 @@
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 export default function Messages() {
+    const {messages, loading} = useGetMessages();
+    console.log('messages' ,messages);
+    console.log("loading ->", loading);
+    
     return (
         <div className="px-4 flex-1 overflow-auto">
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
+            {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+            {!loading && messages.length === 0 && (
+                <p className="text-center">Send message to start the comversation</p>
+            )}
         </div>
     )
 }
