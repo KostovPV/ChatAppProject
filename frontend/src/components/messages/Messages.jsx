f8a7c14aaf1ae6c97407b90a26850599ddd8a999
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-09-22 20:22:47 +0300

    Updated Message , SearchInput and Messages components. extractTime.js implemented to display the time in user-frendly manner

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,18 +1,40 @@
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 export default function Messages() {
-    const {messages, loading} = useGetMessages();
-    console.log('messages' ,messages);
-    console.log("loading ->", loading);
-    
+    const { messages, loading } = useGetMessages();
+    // console.log('messages', messages);
+    // console.log("loading ->", loading);
+    const lastMessageRef = useRef();
+
+
+    useEffect(() => {
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+		}, 100);
+	}, [messages]);
     return (
         <div className="px-4 flex-1 overflow-auto">
+            {!loading &&
+                messages.length > 0 &&
+                messages.map((message) => (
+                    <div key={message._id}
+                        ref={lastMessageRef}
+                    >
+                        <Message message={message} />
+                    </div>
+                )
+
+                )
+
+            }
+
             {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
             {!loading && messages.length === 0 && (
-                <p className="text-center">Send message to start the comversation</p>
+                <p className="text-center">Send message to start the conversation</p>
             )}
         </div>
     )
--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,9 +1,35 @@
 import { IoSearchSharp } from "react-icons/io5";
+import useConversation from "../../zustand/useConversation";
+import { useGetConversations } from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 export default function SearchInput() {
+    const [search, setSearch] = useState('');
+    const {selectedConversation}  = useConversation();
+    const { conversation} = useGetConversations(); 
+
+    const handleSubmit = (e) => {
+            e.preventDefault();
+            if(!search) {
+                return
+            }
+            if(search.length < 3) {
+              return  toast.error('Conversation must have at least 3 symbols')
+            }
+
+
+    }
+
     return (
-        <form className="flex items-center gap-2">
-            <input type="text" placeholder="Search..." className="input input-bordered rounded-full" />
+        <form className="flex items-center gap-2" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Search..."
+                className="input input-bordered rounded-full"
+                value={search}
+                onChange={(e)=> setSearch(e.target.value)}
+            />
             <button type="submot" className="btn btn-circle bg-sky-500 text-white">
                 <IoSearchSharp className='w-6 h-6 outline-none' />
             </button>
