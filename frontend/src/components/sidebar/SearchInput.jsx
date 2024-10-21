a5c2166b678d1c274f8964b11ceae7e929608bc8
KostovPV <106186518+KostovPV@users.noreply.github.com>
2024-10-21 20:19:13 +0300

    Removed commented unnecessary code.

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -45,15 +45,3 @@ export default function SearchInput() {
 }
 
 
-// import { IoSearchSharp } from "react-icons/io5";
-
-// export default function SearchInput() {
-//     return (
-//         <form className="flex items-center gap-2">
-//             <input type="text" placeholder="Search..." className="input input-bordered rounded-full" />
-//             <button type="submot" className="btn btn-circle bg-sky-500 text-white">
-//                 <IoSearchSharp className='w-6 h-6 outline-none' />
-//             </button>
-//         </form>
-//     )
-// }
